b78e0db90980777db2996ae2e3bcd39440b91b30
jrwj97 <jrwj97@aol.com>
2022-01-02 22:35:53 -0500

    added first round of combat

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,26 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10 + 10);
+var fight = function() {
+   window.alert("Welcome to The Robot Gladiators!");
 
-console.log("Our robot's name is " + playerName);
+    enemyHealth = enemyHealth - playerAttack;
 
-function fight() {
-  window.alert("The fight has begun!");
-}
+    console.log(
+      playerName + " attacked " + enemyName + ". " +enemyName + " now has " +enemyHealth + " health remaining."
+    );
 
-// fight();
+    playerHealth = playerHealth - enemyAttack;
+
+    console.log(
+      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    )
+  }
+fight ();
