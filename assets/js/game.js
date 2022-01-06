86bfb95f56f439ea1415a7cf515d95f38b8ec45a
jrwj97 <jrwj97@aol.com>
2022-01-06 16:24:33 -0500

    a

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 100;
+var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
@@ -111,7 +111,7 @@ var startGame = function() {
     }
     // ask the player if they'd like to play again.
     var playAgainConfirm = window.confirm("Would you like to play again");
-    
+
     if (playAgainConfirm) {
       // restart the game
       startGame();
